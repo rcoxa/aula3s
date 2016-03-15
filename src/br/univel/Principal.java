5a62965da047208a440d079bc5af0d970936d36b
Ricardo Cotienschi <ricardocotienschi@live.com>
2016-03-15 20:19:43 -0300

    Alteração a Pedido do cliente.

--- src/br/univel/Principal.java
+++ src/br/univel/Principal.java
@@ -5,7 +5,9 @@ package br.univel;
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Hello");
+		System.out.println("It's me!");
 
 	}
 
